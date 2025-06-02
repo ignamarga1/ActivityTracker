96edc2131a6fb5b08591ca5b49f0216b26de7ded
Ignacio Martínez Gallardo <ignamarga@gmail.com>
2025-06-03 00:48:33 +0200

    Application only accepts Portrait (up) orientation
    There were some bugs when using the landscape orientation. It wasn't supposed to be supported anyways, but I forgot to lock the app orientation

--- activity_tracker_flutter/lib/main.dart
+++ activity_tracker_flutter/lib/main.dart
@@ -8,11 +8,15 @@ import 'package:activity_tracker_flutter/themes/light_mode.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, // Normal Portrait
+  ]);
   runApp(const MyApp());
 }
 
