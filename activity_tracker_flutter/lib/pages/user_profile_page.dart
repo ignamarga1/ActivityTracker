a1fd3b5d0b95295dedd66f6faa5d8c3bf3f00a8a
Ignacio Martínez Gallardo <ignamarga@gmail.com>
2025-06-04 10:36:49 +0200

    Basic user profile page

--- activity_tracker_flutter/lib/pages/user_profile_page.dart
+++ activity_tracker_flutter/lib/pages/user_profile_page.dart
@@ -1,15 +1,224 @@
 import 'package:activity_tracker_flutter/components/home_drawer.dart';
+import 'package:activity_tracker_flutter/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Perfil')),
+      appBar: AppBar(
+        title: const Text('Perfil'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            tooltip: 'Editar perfil',
+            onPressed: () {
+              Navigator.pushNamed(context, '/editUserProfile');
+            },
+          ),
+        ],
+      ),
       backgroundColor: Theme.of(context).colorScheme.surface,
-      drawer: HomeDrawer(),
+      drawer: const HomeDrawer(),
+      body: user == null
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  CircularProgressIndicator(color: Colors.white),
+                  SizedBox(height: 12),
+                  Text(
+                    'Cargando datos...',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            )
+          : SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+
+                  // Profile picture
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: user.profilePictureURL != ''
+                        ? NetworkImage(user.profilePictureURL!)
+                        : null,
+                    backgroundColor: Colors.grey.shade700,
+                    child: user.profilePictureURL == ''
+                        ? const Icon(
+                            Icons.person,
+                            size: 80,
+                            color: Colors.white,
+                          )
+                        : null,
+                  ),
+                  const SizedBox(height: 12),
+
+                  // Username
+                  Text(
+                    '@${user.username}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+
+                  // User Data section
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Mis datos',
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Nickname
+                      Text.rich(
+                        TextSpan(
+                          text: 'Apodo: ',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: user.nickname,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Email
+                      Text.rich(
+                        TextSpan(
+                          text: 'Correo electrónico: ',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: user.email,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Account creation date
+                      Text.rich(
+                        TextSpan(
+                          text: 'Fecha de creación: ',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: DateFormat(
+                                'd/M/y, HH:mm',
+                              ).format(user.createdAt.toDate()),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 40),
+
+                  // User Stadistics section
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Mis estadísticas',
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 280),
+
+                  // Delete account button
+                  SizedBox(
+                    width: double.infinity,
+                    child: FilledButton(
+                      style: FilledButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        padding: const EdgeInsets.all(20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () async {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text(
+                                '¿Estás seguro de que quieres eliminar tu cuenta? \nToda tu información será eliminada y no se podrá recuperar',
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Sí'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('No'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: const Text(
+                        'Eliminar cuenta',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
